c1cb5c949f47271b31f96a2d41757994e4e7960b
Richard Batista <36830488+blue-stalfos@users.noreply.github.com>
2019-02-05 20:01:00 -0500

    Completed second half of lesson

--- src/com/richardbatista/Main.java
+++ src/com/richardbatista/Main.java
@@ -7,6 +7,7 @@ public class Main {
 
         Dog dog = new Dog("Yorkie", 8, 20, 2, 4, 1, 20, "long silky");
         dog.eat(); // Inherited from Animal class
-
+        dog.walk();
+        dog.run();
     }
 }
